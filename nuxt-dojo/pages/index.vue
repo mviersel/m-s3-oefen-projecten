2a26b76f749d44ecb0eba8a7a2d0143b194c22ca
Martijn <512214@student.fontys.nl>
2024-05-04 15:59:52 +0200

    Les 1

---
+++ nuxt-dojo/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit natus
+      labore veritatis tempore animi doloribus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit natus
+      labore veritatis tempore animi doloribus!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p{
+    margin: 20px 0;
+}
+</style>
